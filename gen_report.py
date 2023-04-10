a40acdc1a43977ff2368e2973eecd778c1a3241a
Julien uj Abadji <sudo.screenlife@gmail.com>
2023-04-10 12:11:24 +0000

    change report dir

--- gen_report.py
+++ gen_report.py
@@ -5,7 +5,7 @@ from ydata_profiling import ProfileReport
 from datetime import datetime
 from pathlib import Path
 
-def gen_report(measures: Path = "res/measures.jsonl", out: Path = "res/static/report.html"):
+def gen_report(measures: Path = "res/measures.jsonl", out: Path = "report.html"):
     df = pl.read_ndjson(measures)
     # df = df.with_columns(pl.col("timestamp").str.strptime(pl.Date, "%+", strict=False))
     df = df.with_columns(pl.col("timestamp").apply(lambda x: datetime.fromisoformat(x)))
